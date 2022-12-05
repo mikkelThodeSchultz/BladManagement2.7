fc74debf2394ad24115ad7d97e78964b1ae1b069
annesofiefogh <anne2z08@stud.kea.dk>
2022-12-05 09:31:59 +0100

    Email functionality

--- src/main/java/com/example/cykelrytter/controllers/AdminController.java
+++ src/main/java/com/example/cykelrytter/controllers/AdminController.java
@@ -1,13 +1,40 @@
 package com.example.cykelrytter.controllers;
 
+import com.example.cykelrytter.model.NewsList;
 import com.example.cykelrytter.services.IAdminService;
 import com.example.cykelrytter.services.IArtistService;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cykelrytter.services.INewsListService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
+
+@CrossOrigin
 @RestController
 public class AdminController {
     private IAdminService adminService;
-    public AdminController(IAdminService adminService){
+    private INewsListService newsListService;
+
+    public AdminController(IAdminService adminService, INewsListService newsListService) {
         this.adminService = adminService;
+        this.newsListService = newsListService;
+    }
+
+    @GetMapping("/get/allEmails")
+    public ResponseEntity<Set<NewsList>> getAll(){
+        return new ResponseEntity<>(newsListService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create/emailToList")
+    public ResponseEntity<NewsList> create(@RequestBody NewsList newsList){
+        return new ResponseEntity<>(newsListService.save(newsList), HttpStatus.OK);
+    }
+
+    //Så kan jeg tilføje et id til url'en i frontend
+    @DeleteMapping("/delete/emailFromList/{id}")
+    public ResponseEntity<String> delete (@PathVariable("id") Long emailId){
+        newsListService.deleteById(emailId);
+        return new ResponseEntity<>("Du er nu frameldt vores nyhedsbrev", HttpStatus.OK);
     }
 }
